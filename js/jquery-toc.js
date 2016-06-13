0e50d52465ebe6daf8b7c0328be890775e1c4301
Sylvain Marty <sylvain.marty@aol.fr>
2016-06-13 17:00:27 +0200

    S'arrête avant un élément lors du scrolling

--- js/jquery-toc.js
+++ js/jquery-toc.js
@@ -43,7 +43,7 @@
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html, body').animate({
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top- target.height()
             }, animationSpeed);
             return false;
           }
